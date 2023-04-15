e8ed6015483dbf7c3d9e3506011969c53be4651a
jefferson echavarria <jfrson__@live.com>
2023-04-15 06:55:41 -0500

    Add project files.

---
+++ TicketSystem_Concert/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TicketSystem_Concert.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
