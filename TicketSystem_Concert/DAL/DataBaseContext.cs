5250104c58b01675506f09ca2db0966159847da0
jefferson echavarria <jfrson__@live.com>
2023-04-15 08:09:27 -0500

    A new project is created, the DAL folder and the ticket.cs class are created, the connection to the database is created.

---
+++ TicketSystem_Concert/DAL/DataBaseContext.cs
@@ -0,0 +1,25 @@
+﻿using TicketSystem_Concert.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace TicketSystem_Concert.DAL
+{
+	public class DataBaseContext : DbContext
+	{
+		#region Builder
+		public DataBaseContext(DbContextOptions<DataBaseContext> option) : base(option)
+		{
+		}
+		#endregion
+
+		#region Properties
+		public DbSet<Ticket> Tickets { get; set; }
+		#endregion
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<Ticket>().HasIndex(t => t.Id).IsUnique();
+		}
+	}
+}
---
+++ TicketSystem_Concert/DAL/Entities/Ticket.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketSystem_Concert.DAL.Entities
+{
+	public class Ticket
+	{
+		#region Properties
+		[Display(Name = "Ticket.")]
+		[Key]
+		[MaxLength(100)]
+		[Required(ErrorMessage = "El campo [0] es necesario.")]
+		public string? Id { get; set; }
+
+		[Display(Name = "Fecha de uso de la boleta.")]
+		public DateTime? UseData { get; set; }
+
+		[Display(Name = "Uso la boleta.")]
+		public bool? IsUsed { get; set; }
+
+		[Display(Name = "Portería de ingreso.")]
+		public bool? EntranceGate { get; set; }
+		#endregion
+	}
+}
